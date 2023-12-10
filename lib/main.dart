63c5e2b730cfb514fddbebef41469eab82956a85
Abhishek kumar <99316496+abhi999999@users.noreply.github.com>
2023-12-10 07:12:49 +0530

    5 images per user

--- lib/main.dart
+++ lib/main.dart
@@ -137,45 +137,47 @@ class _SignupPageState extends State<SignupPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextField(
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            SizedBox(height: 12.0),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Password'),
-            ),
-            SizedBox(height: 12.0),
-            TextField(
-              controller: usernameController,
-              decoration: InputDecoration(labelText: 'Username'),
-            ),
-            SizedBox(height: 12.0),
-            TextField(
-              controller: mobileNumberController,
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(labelText: 'Mobile Number'),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: _signup,
-              child: Text('Signup'),
-            ),
-            SizedBox(height: 12.0),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context); // Go back to the login page
-              },
-              child: Text('Already have an account? Login'),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              SizedBox(height: 12.0),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(labelText: 'Password'),
+              ),
+              SizedBox(height: 12.0),
+              TextField(
+                controller: usernameController,
+                decoration: InputDecoration(labelText: 'Username'),
+              ),
+              SizedBox(height: 12.0),
+              TextField(
+                controller: mobileNumberController,
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(labelText: 'Mobile Number'),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: _signup,
+                child: Text('Signup'),
+              ),
+              SizedBox(height: 12.0),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context); // Go back to the login page
+                },
+                child: Text('Already have an account? Login'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/upload_images.dart
+++ lib/upload_images.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+// import 'package:image_picker/image_picker.dart';
+import 'package:file_picker/file_picker.dart';
 
 class UploadScreen extends StatefulWidget {
   @override
@@ -13,32 +14,52 @@ class UploadScreen extends StatefulWidget {
 class _UploadScreenState extends State<UploadScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseStorage _storage = FirebaseStorage.instance;
-  final ImagePicker _imagePicker = ImagePicker();
+  // final ImagePicker _imagePicker = ImagePicker();
 
-  XFile? _pickedFile;
-  bool _isLoading = false;
+  // XFile? _pickedFile;
+  // bool _isLoading = false;
   int uploadLimit = 5;
+  List<File>? _pickedFile;
+  bool _isLoading = false;
+  // Future<void> _getImage() async {
+  //   try {
+  //     final image = await _imagePicker.pickImage(source: ImageSource.gallery);
+
+  //     setState(() {
+  //       _pickedFile = image;
+  //     });
+  //   } catch (e) {
+  //     print('Error picking image: $e');
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       SnackBar(
+  //         content: Text('An error occurred while picking the image: $e'),
+  //       ),
+  //     );
+  //   }
+  // }
 
   Future<void> _getImage() async {
     try {
-      final image = await _imagePicker.pickImage(source: ImageSource.gallery);
-
-      setState(() {
-        _pickedFile = image;
-      });
+      final result = await await FilePicker.platform
+          .pickFiles(allowMultiple: true, type: FileType.image);
+      if (result != null) {
+        setState(() {
+          _pickedFile = result.paths.map((path) => File(path!)).toList();
+        });
+      }
     } catch (e) {
-      print('Error picking image: $e');
+      print('Error picking file: $e');
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('An error occurred while picking the image: $e'),
+          content: Text('An error occurred while picking the file: $e'),
         ),
       );
     }
   }
 
-  Future<void> _uploadImage() async {
+  Future<void> _uploadImages() async {
     try {
-      if (_pickedFile != null) {
+      if (_pickedFile != null && _pickedFile!.isNotEmpty) {
         final user = _auth.currentUser;
         final userId = user?.uid;
 
@@ -47,7 +68,7 @@ class _UploadScreenState extends State<UploadScreen> {
         final ListResult result = await userRef.list();
         final int currentImageCount = result.items.length;
 
-        if (currentImageCount >= uploadLimit) {
+        if (currentImageCount + _pickedFile!.length > uploadLimit) {
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(
               content: Text('Upload limit reached for this user.'),
@@ -60,43 +81,49 @@ class _UploadScreenState extends State<UploadScreen> {
           _isLoading = true;
         });
 
-        final fileName = 'user_$userId$currentImageCount.jpg';
-        final reference = _storage.ref().child('images/$fileName');
+        for (int i = 0; i < _pickedFile!.length; i++) {
+          final fileName = 'user_$userId${currentImageCount + i}.jpg';
+          final reference = _storage.ref().child('images/$fileName');
 
-        // Check image file extension and convert to JPEG if necessary
-        File imageFile = File(_pickedFile!.path);
-        final imageFormat = imageFile.path.split('.').last.toLowerCase();
+          File imageFile = _pickedFile![i];
+          final imageFormat = imageFile.path.split('.').last.toLowerCase();
 
-        if (imageFormat != 'jpg' && imageFormat != 'jpeg') {
-          final convertedImage = await imageFile.readAsBytes();
-          final newFile = await File.fromUri(Uri.file(
-            '${imageFile.path}.temp.jpg',
-          ));
-          await newFile.writeAsBytes(convertedImage);
-          imageFile = newFile;
+          if (imageFormat != 'jpg' && imageFormat != 'jpeg') {
+            final convertedImage = await imageFile.readAsBytes();
+            final newFile = await File.fromUri(Uri.file(
+              '${imageFile.path}.temp.jpg',
+            ));
+            await newFile.writeAsBytes(convertedImage);
+            imageFile = newFile;
+          }
+
+          await reference.putFile(imageFile);
+
+          // Update the user's image count after a successful upload
+          await userRef.child(fileName).putFile(imageFile);
         }
 
-        await reference.putFile(imageFile);
-
-        // Update the user's image count after a successful upload
-        await userRef.child(fileName).putFile(imageFile);
-
-        print('Image uploaded successfully.');
+        print('Images uploaded successfully.');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Images uploaded successfully.'),
+          ),
+        );
         setState(() {
           _isLoading = false;
         });
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            content: Text('Please select an image before uploading.'),
+            content: Text('Please select images before uploading.'),
           ),
         );
       }
     } catch (e) {
-      print('Error uploading image: $e');
+      print('Error uploading images: $e');
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('An error occurred while uploading the image: $e'),
+          content: Text('An error occurred while uploading the images: $e'),
         ),
       );
       setState(() {
@@ -104,6 +131,78 @@ class _UploadScreenState extends State<UploadScreen> {
       });
     }
   }
+
+  // Future<void> _uploadImage() async {
+  //   try {
+  //     if (_pickedFile != null) {
+  //       final user = _auth.currentUser;
+  //       final userId = user?.uid;
+
+  //       // Check the number of uploaded images for the user
+  //       final userRef = _storage.ref().child('users/$userId');
+  //       final ListResult result = await userRef.list();
+  //       final int currentImageCount = result.items.length;
+
+  //       if (currentImageCount >= uploadLimit) {
+  //         ScaffoldMessenger.of(context).showSnackBar(
+  //           SnackBar(
+  //             content: Text('Upload limit reached for this user.'),
+  //           ),
+  //         );
+  //         return;
+  //       }
+
+  //       setState(() {
+  //         _isLoading = true;
+  //       });
+
+  //       final fileName = 'user_$userId$currentImageCount.jpg';
+  //       final reference = _storage.ref().child('images/$fileName');
+
+  //       // Check image file extension and convert to JPEG if necessary
+  //       File imageFile = File(_pickedFile![0].path);
+  //       final imageFormat = imageFile.path.split('.').last.toLowerCase();
+
+  //       if (imageFormat != 'jpg' && imageFormat != 'jpeg') {
+  //         final convertedImage = await imageFile.readAsBytes();
+  //         final newFile = await File.fromUri(Uri.file(
+  //           '${imageFile.path}.temp.jpg',
+  //         ));
+  //         await newFile.writeAsBytes(convertedImage);
+  //         imageFile = newFile;
+  //       }
+
+  //       await reference.putFile(imageFile);
+
+  //       // Update the user's image count after a successful upload
+  //       await userRef.child(fileName).putFile(imageFile);
+
+  //       print('Image uploaded successfully.');
+  //       SnackBar(
+  //         content: Text('Image uploaded successfully.'),
+  //       );
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //     } else {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         SnackBar(
+  //           content: Text('Please select an image before uploading.'),
+  //         ),
+  //       );
+  //     }
+  //   } catch (e) {
+  //     print('Error uploading image: $e');
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       SnackBar(
+  //         content: Text('An error occurred while uploading the image: $e'),
+  //       ),
+  //     );
+  //     setState(() {
+  //       _isLoading = false;
+  //     });
+  //   }
+  // }
 
   Future<void> _logout(BuildContext context) async {
     await _auth.signOut();
@@ -126,9 +225,9 @@ class _UploadScreenState extends State<UploadScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            _pickedFile != null
-                ? Expanded(child: Image.file(File(_pickedFile!.path)))
-                : Text('No image selected'),
+            if (_pickedFile != null)
+              for (final file in _pickedFile!)
+                Expanded(child: Image.file(File(file.path!))),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: _isLoading ? null : _getImage,
@@ -136,7 +235,7 @@ class _UploadScreenState extends State<UploadScreen> {
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _isLoading ? null : _uploadImage,
+              onPressed: _isLoading ? null : _uploadImages,
               child: Text('Upload Image'),
             ),
             if (_isLoading) CircularProgressIndicator(),
